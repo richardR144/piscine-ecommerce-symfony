d2cf9cf7efbc0560303f1b9ef33e6ad6f6733fee
richardR144 <richard.rigollet@lapiscine.pro>
2025-05-09 13:25:07 +0200

    création de la page Admin

--- src/Controller/admin/AdminProductController.php
+++ src/Controller/admin/AdminProductController.php
@@ -13,17 +13,18 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class AdminProductController extends AbstractController {
 
-
+    //je créais une route pour afficher la page de création de produit url(/admin/create-product)
 	#[Route('/admin/create-product', name: 'admin-create-product')]
+    //je fais appel au repository de la catégorie pour récupérer toutes les catégories
 	public function displayCreateProduct(CategoryRepository $categoryRepository, Request $request) {
-
+        //je créais une méthode pour afficher la page de création de produit ('POST')
 		if ($request->isMethod('POST')) {
-
+        //je récupère les données du formulaire
 			$title = $request->request->get('title');
 			$description = $request->request->get('description');
 			$price = $request->request->get('price');
 			$categoryId = $request->request->get('category-id');
-			
+			//si le champ is-published est coché, je le met à true sinon false
 			if ($request->request->get('is-published') === 'on') {
 				$isPublished = true;
 			} else {
@@ -31,10 +32,16 @@ class AdminProductController extends AbstractController {
 			}
 		}
 
+        //je récupère la liste des catégories
+        //je fais appel au repository de la catégorie pour récupérer toutes les catégories
+
 		$categories = $categoryRepository->findAll();
 
-
+        //je fais un dump pour voir si je récupère bien les catégories
+        //dump($categories);
+        //je retourne la vue create-product.html.twig et je fais un render pour afficher la page
 		return $this->render('admin/product/create-product.html.twig', [
+            //je passe la liste des catégories à la vue
 			'categories' => $categories
 		]);
 	}
